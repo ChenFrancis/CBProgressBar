edba495558b1fa16a2943d96abf2b4f98ebf72f0
ChenFrancis <cb331339299@163.com>
2014-02-24 00:36:46 -0800

    加入CBProgressBar

---
+++ CBProgressBar/CBProgressBar/CBProgressBar/CBProgressBar.m
@@ -0,0 +1,13 @@
+//
+//  CBProgressBar.m
+//  CBProgressBar
+//
+//  Created by xychen on 14-2-24.
+//  Copyright (c) 2014年 CB. All rights reserved.
+//
+
+#import "CBProgressBar.h"
+
+@implementation CBProgressBar
+
+@end
